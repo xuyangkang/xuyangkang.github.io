a9880195c95b38e182526811df931a190a390965
Xuyang Kang <xuyangkang@gmail.com>
2024-07-17 22:29:00 +0100

    minify the html

---
+++ minify.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true index_dev.html -o index.html
