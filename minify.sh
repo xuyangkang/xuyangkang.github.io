d0c319e3b7a20a12725e4ca159b1094f4d1e15a3
Xuyang Kang <xuyangkang@gmail.com>
2024-07-17 22:31:51 +0100

    Add spaces in tag

--- minify.sh
+++ minify.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true index_dev.html -o index.html
+html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes ---use-short-doctype --minify-css true --minify-js true index_dev.html -o index.html
